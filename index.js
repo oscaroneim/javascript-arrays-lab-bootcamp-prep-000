a81644a41b08b06a816acba41f33ddc544e77ae3
Oscar Oneim <oscar4londres@gmail.com>
2020-02-20 17:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  [...kittens,name];
-  return kittens;
+  return [...kittens,name];
 }
 
 function prependKitten(name) {
