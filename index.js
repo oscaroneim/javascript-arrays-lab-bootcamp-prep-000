3473683c39da8076faf81c1c251296b635f38055
Oscar Oneim <oscar4londres@gmail.com>
2020-02-20 16:59:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  kittens.append()
+  kittens.push(name);
+  return kittens;
 }
